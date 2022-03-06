1d66da32dcf453f104e2eb0615c9153a47461238
Frcomparan <frcomparan@gmail.com>
2022-03-05 21:39:47 -0600

    Finish 'about_dice_project' koan 215/281 (76%)

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -4,9 +4,14 @@ require File.expand_path("#{File.dirname(__FILE__)}/neo")
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+
+  attr_accessor :values
+
+  def roll(num)
+    @values = (0...num).collect { 1 + rand(6)}
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -20,13 +25,14 @@ class AboutDiceProject < Neo::Koan
     dice.roll(5)
     assert dice.values.is_a?(Array), 'should be an array'
     assert_equal 5, dice.values.size
-    dice.each_value do |value|
+    dice.values.each do |value|
       assert value >= 1 && value <= 6, "value #{value} must be between 1 and 6"
     end
   end
 
   def test_dice_values_do_not_change_unless_explicitly_rolled
     dice = DiceSet.new
+
     dice.roll(5)
     first_time = dice.values
     second_time = dice.values
